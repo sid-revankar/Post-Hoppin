a101ffe244570d10b3d01a66bd3f57a98081f44f
SpoodoW <sripadjoshi2001@yahoo.in>
2024-07-25 19:17:12 +0530

    Created the dev branch and changed application structure

--- posthoppin/main.py
+++ posthoppin/main.py
@@ -1,31 +1,7 @@
 import typer
-from typing import Optional
 
-from posthoppin import __app_name__, __version__
-
-app = typer.Typer()
-
-def version_callback(value: bool) -> None:
-    if value:
-        app_name_version = {
-            "name": typer.style(__app_name__,fg=typer.colors.BRIGHT_YELLOW),
-            "version": typer.style(__version__,fg=typer.colors.BRIGHT_GREEN),
-        }
-        typer.echo(f"{app_name_version['name']} {app_name_version['version']}")
-        raise typer.Exit()
-
-@app.callback()
-def main(
-    version: Optional[bool] = typer.Option(
-        None,
-        "--version",
-        "-v",
-        help="Show the application's name and version then exit.",
-        callback=version_callback,
-        is_eager=True,
-    )
-) -> None:
-    return
+def main():
+    print("hello world!")
 
 if __name__ == '__main__':
     typer.run(main)
