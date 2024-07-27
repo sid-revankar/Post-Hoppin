1e34948d6174b66eb061c882be471e17dc5c4e87
sid-revankar <siddharthrevankar00@gmail.com>
2024-07-28 01:03:17 +0530

    feat: File Handling Automation

--- posthoppin/main.py
+++ posthoppin/main.py
@@ -1,8 +1,31 @@
 import typer
-from view import View
+import os
+import shutil
+from models import File_Handling
+import time
 
 def main():
-    myView = View()
-    myView.fileInput()
-if __name__ == '__main__':
+    path = input("Enter the path: ")
+    file_handle = File_Handling(path)
+
+    avg_time = 0.0
+    for _ in range(10):
+        if os.path.exists("/home/fastaf/cli project/posthoppin/.config"):
+            shutil.rmtree(
+                "/home/fastaf/cli project/posthoppin/.config", ignore_errors=True
+            )
+
+    start_time = time.time()
+    folder_path = file_handle.get_path()
+    path_list = file_handle.dir_sort(folder_path)
+
+    File_Handling.dir_keyValuePair = file_handle.passTree(path_list)
+
+    end_time = time.time() - start_time
+    avg_time += end_time
+
+
+    print(f"execution compelete: {(avg_time*1000)/10:.2f}'s ")
+
+if __name__ == "__main__":
     typer.run(main)
