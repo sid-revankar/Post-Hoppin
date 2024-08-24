0917c0e4e45ffb38c7f755c1336d381c36ccdaa6
sid-revankar <siddharthrevankar00@gmail.com>
2024-08-24 14:32:28 +0530

    Corrections

--- posthoppin/main.py
+++ posthoppin/main.py
@@ -1,31 +1,60 @@
 import typer
-import os
-import shutil
-from models import File_Handling
-import time
+from rich import print as show
+from typing import Optional
+from backend import FileHandling
+import config
 
-def main():
-    path = input("Enter the path: ")
-    file_handle = File_Handling(path)
-
-    avg_time = 0.0
-    for _ in range(10):
-        if os.path.exists("/home/fastaf/cli project/posthoppin/.config"):
-            shutil.rmtree(
-                "/home/fastaf/cli project/posthoppin/.config", ignore_errors=True
-            )
-
-    start_time = time.time()
-    folder_path = file_handle.get_path()
-    path_list = file_handle.dir_sort(folder_path)
-
-    File_Handling.dir_keyValuePair = file_handle.passTree(path_list)
-
-    end_time = time.time() - start_time
-    avg_time += end_time
+app = typer.Typer()
 
 
-    print(f"execution compelete: {(avg_time*1000)/10:.2f}'s ")
+def version(value: bool) -> None:
+    if value:
+        ver_name = {
+            "name": config.NAME,
+            "version": config.version,
+        }
+        show(
+            f":rabbit: [bold magenta1]{ver_name['name']}[/bold magenta1] [green]v{ver_name['version']}[/green]"
+        )
+        raise typer.Exit()
+
+
+@app.callback()
+def main(
+    greet: str,
+    path: str = typer.Option(
+        "",
+        "--copy",
+        "-c",
+        help='Copies the entire directory >> .config \n **NOTE: path should be in " " Quotes.',
+    ),
+    version: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        "-v",
+        help="Shows Version",
+        callback=version,
+        is_eager=True,
+    ),
+):
+    """
+    If you say "yo" it will greet!
+    else executes normally.
+    """
+    user_path = path
+    match greet:
+        case "yo":
+            show("[red]Hello User[/red] :ghost:")
+        case _:
+            pass
+    file_handle = FileHandling(user_path)
+
+    folders_path = file_handle.getPath()
+    path_dir_list = file_handle.getSubDir(folders_path)
+    path_file_list = file_handle.getSubDir_file(folders_path)
+
+    file_handle.putFiles(path_dir_list, path_file_list)
+
 
 if __name__ == "__main__":
     typer.run(main)
